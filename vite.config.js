320b9747996093998d3d4a29c87ba80f934cb4c9
reggggg <reginaldpabilonia@gmail.com>
2024-08-17 20:48:14 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,33 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+// import { defineConfig } from 'vite';
+// import { resolve } from 'path';
+// import vercel from 'vite-plugin-vercel';
 
-export default defineConfig({
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        manualChunks: undefined,
-      },
+// export default defineConfig({
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'index.html'),
+//       },
+//       output: {
+//         manualChunks: undefined,
+//       },
+//     },
+//   },
+//   server: {
+//     historyApiFallback: true,
+//   },
+//   plugins: [vercel()],
+// });
+
+
+import { defineConfig, loadEnv } from 'vite'
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-  },
-  server: {
-    historyApiFallback: true,
-  },
-});
+  }
+})
