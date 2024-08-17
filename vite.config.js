7f277467baad727c542765f841f2a146ab5f2744
reggggg <reginaldpabilonia@gmail.com>
2024-08-17 20:51:55 +0800

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,3 @@
-// import { defineConfig } from 'vite';
-// import { resolve } from 'path';
-// import vercel from 'vite-plugin-vercel';
-
-// export default defineConfig({
-//   build: {
-//     outDir: 'dist',
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, 'index.html'),
-//       },
-//       output: {
-//         manualChunks: undefined,
-//       },
-//     },
-//   },
-//   server: {
-//     historyApiFallback: true,
-//   },
-//   plugins: [vercel()],
-// });
-
-
 import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ command, mode }) => {
