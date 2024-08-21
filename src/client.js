60284faf625054e84c8372545ef57ccd3111d9a9
Reginald Pabiloina <reginaldpabilonia@gmail.com>
2024-08-21 10:53:43 +0800

    fix: authoriazation header

--- src/client.js
+++ src/client.js
@@ -1,19 +1,31 @@
 import axios from 'axios';
 
-export const strapi = axios.create({
-  baseURL: import.meta.env.VITE_STRAPI_API_URL,
+const strapi = axios.create({
+	baseURL: import.meta.env.VITE_STRAPI_API_URL,
 	headers: {
 		'Content-Type': 'application/json',
 		'Accept': 'application/json',
 	},
 })
 
-export const strapi_auth = axios.create({
-  baseURL: import.meta.env.VITE_STRAPI_API_URL,
+const strapi_auth = axios.create({
+	baseURL: import.meta.env.VITE_STRAPI_API_URL,
 	headers: {
 		'Content-Type': 'application/json',
 		'Accept': 'application/json',
-		Authorization: `Bearer ${localStorage.getItem('token')}`
 	},
 })
+
+strapi_auth.interceptors.request.use((config) => {
+	const token = localStorage.getItem('token');
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+	return config;
+});
+
+export {
+	strapi,
+	strapi_auth
+}
 
