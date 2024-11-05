476b3702bff7bb71138747eb0dafe5cfbe9885ae
reggggg <reginaldpabilonia@gmail.com>
2024-11-05 20:42:55 +0800

    update: inject data function to accept booth numbers more than 99

--- src/floorplans.js
+++ src/floorplans.js
@@ -27,7 +27,7 @@ export async function getFloorplan(conferenceId) {
 
 export function generateDataToInjectInFloorplan() {
   return new Promise((resolve) => {
-    const max_count = 99;
+    const max_count = 200;
     const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
     const containerElement = document.getElementById('Booths');
 
